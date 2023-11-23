ba37077d74e0ada844210b8a71b173023cfcabdb
kluyko-pavel <kluyko.pasha@mail.ru>
2023-11-23 16:56:41 +0300

    add readme.md & change callback api url

---
+++ README.md
@@ -0,0 +1,5 @@
+### Test Next.js project
+
+[DemoLink](https://test-next-js-iota-khaki.vercel.app/)
+
+A full-featured application for familiarization with Next.js using Node.js and Express to implement the callback function.
--- src/constants.ts
+++ src/constants.ts
@@ -8,4 +8,5 @@ export const swiperArr: ISwiperSlide[] = [
 
 export const POSTS_URL =
   "https://63e9269e811db3d7eff7f6ff.mockapi.io/cosmetics";
-export const CALLBACK_URL = "http://localhost:5000/api/send-email";
+export const CALLBACK_URL =
+  "https://test-next-js-server.onrender.com/api/send-email";
